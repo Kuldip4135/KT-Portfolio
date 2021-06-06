5401e8715f7198de3f19dc319a0a6888a9f7ad46
Kuldip4135 <thakkarkuldip29@gmail.com>
2021-06-06 11:02:59 +0530

    Project Section Changed

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const header = document.querySelector(".header");
+const navLinks = document.querySelectorAll("ul li");
 
 hamburgerMenu.addEventListener("click", () => {
   header.classList.toggle("header--active");
+});
+
+navLinks.forEach((navLink) => {
+  navLink.addEventListener("click", () => {
+    if (header.classList.contains("header--active")) {
+      header.classList.remove("header--active");
+    }
+  });
 });
 
 window.addEventListener("scroll", () => {
