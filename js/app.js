9db04237feafbc0f7f6b98ee6b9baf17e88b91eb
Kuldip4135 <thakkarkuldip29@gmail.com>
2021-06-06 20:19:13 +0530

    About Section Added

--- js/app.js
+++ js/app.js
@@ -14,7 +14,28 @@ navLinks.forEach((navLink) => {
   });
 });
 
+//Header
 window.addEventListener("scroll", () => {
   let windowPosition = window.scrollY > 200;
   header.classList.toggle("scroll--active", windowPosition);
 });
+
+//Random Quotes
+const quote = document.querySelector(".quote");
+const author = document.querySelector(".quote-author");
+const URL = "https://type.fit/api/quotes";
+
+const getOneQuote = (resData) => {
+  const oneQuote = Math.floor(Math.random() * 1643);
+  quote.innerText = resData[oneQuote].text;
+  author.innerText = `- ${resData[oneQuote].author}`;
+};
+
+const getRandomQuotes = async () => {
+  quote.innerText = "Getting Quote For U";
+  const res = await fetch(URL);
+  const resData = await res.json();
+  getOneQuote(resData);
+};
+
+getRandomQuotes();
